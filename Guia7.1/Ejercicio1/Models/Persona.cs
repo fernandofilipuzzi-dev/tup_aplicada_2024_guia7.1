0c74427a89cfadacde010282c1a5ae25744a5872
fernandofilipuzzi-dev <fernandofilipuzzi.dev@gmail.com>
2024-09-23 23:49:16 -0300

    init repo

---
+++ Guia7.1/Ejercicio1/Models/Persona.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections;
+using System.Net.Mime;
+
+namespace Ejercicio1.Models
+{
+    public class Persona
+    {
+        int dni;
+        public int DNI 
+        {
+            get 
+            {
+                return dni;
+            }
+            set
+            {
+                if (value <= 1000000)
+                    throw new RangoDniIncorrectoException();
+                dni = value;
+            }
+        }
+        public string Nombre { get; set; }
+
+        public Persona(int dni, string nombre)
+        {
+            DNI = dni;
+            Nombre = nombre;
+        }
+    }
+}
